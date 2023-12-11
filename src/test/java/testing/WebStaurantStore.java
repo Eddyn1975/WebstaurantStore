d4a23ac7a075990b2b312e8335e17d6e11d0e27b
Eddy <eddyn1975@gmail.com>
2023-12-10 18:16:59 -0600

    Updated the code to take care of the presence of type menu before clicking the add cart button

--- src/test/java/testing/WebStaurantStore.java
+++ src/test/java/testing/WebStaurantStore.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,6 +17,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class WebStaurantStore {
 	static WebDriver driver;
 	static WebDriverWait wait;
+	static JavascriptExecutor jes;
 	public static void main(String[] args) throws InterruptedException {
 
 		try {
@@ -30,7 +34,7 @@ public class WebStaurantStore {
 		// 2. Search for 'stainless work table'.
         
 		WebElement searchInput = driver.findElement(By.id("searchval"));
-        searchInput.sendKeys("stainless work table");
+		searchInput.sendKeys("stainless work table");
         searchInput.submit();
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
         
@@ -45,76 +49,139 @@ public class WebStaurantStore {
             }
         }
         System.out.println("All products contain the word 'Table': " + allProductsContainTable);
-         // ScrollDown to the bottom of the page
-        JavascriptExecutor jes = (JavascriptExecutor) driver;
+         
+        // 4.Found the last item and Add it to the cart
+        
+        // ScrollDown to the bottom of the page
+        
+        wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+        jes = (JavascriptExecutor) driver;
         jes.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        
         // Locate the pagination component
-        WebElement paginationContainer = driver.findElement(By.xpath("//div[@id='paging']"));
+        
+        wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+        WebElement paginationContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='paging']")));
+        
         // Get all the page number elements
+        
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
         List<WebElement> pageNumbers = paginationContainer.findElements(By.tagName("a"));
+        
         // Find the last page element
+        
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
         WebElement lastPage = pageNumbers.get(pageNumbers.size() - 2);
-       // Click the last page element
+       
+        // Click the last page element
+        
         lastPage.click();
-        //wait = new WebDriverWait(driver, Duration.ofSeconds(100));
-        Thread.sleep(100);
-        // Scroll down
-        jes.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        // 4.found the last item and Add it to the cart
-        // Find all elements with the class attribute value "add-to-cart"
+        
         wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+        
+        // Find all elements with the class attribute value "add-to-cart"
+        
         List<WebElement> elements = driver.findElements(By.xpath("//div[@class='add-to-cart']"));
+
         // Check if the elements list is not empty
+        
         if (!elements.isEmpty()) {
-            // Get the last element from the list
-            WebElement lastElement = elements.get(elements.size() - 1);
-            // Interact with the last element as needed
-            lastElement.click(); // Example: Click on the last element
+            
+        	// Get the last element from the list
+            
+        	WebElement lastElement = elements.get(elements.size() - 1);
+            
+            // Check if the Type menu exists for the last found item
+            
+            List<WebElement> typeMenu = driver.findElements(By.xpath("//select[@name='item_number']"));
+            if (typeMenu.isEmpty()) {
+                
+            	// Type menu does not exist, click on Add to Cart Button 
+                
+            	lastElement.click();
+            } else {
+                
+            	// Type menu exists, click on it and select an option
+                
+            	WebElement typeMenuDropdown = typeMenu.get(typeMenu.size() - 1);
+                Select typeMenuSelect = new Select(typeMenuDropdown);
+                
+                // Replace "Sink on Left" with the common part of the desired option text
+                
+                String optionTextPrefix = "Sink on Left";
+
+                // Loop through all the options and select the desired option that starts with the prefix
+                
+                boolean optionFound = false;
+                for (WebElement option : typeMenuSelect.getOptions()) {
+                    String optionText = option.getText();
+                    if (optionText.startsWith(optionTextPrefix)) {
+                        typeMenuSelect.selectByVisibleText(optionText);
+                        optionFound = true;
+                        break;
+                    }
+                }
+
+                if (!optionFound) {
+                    System.out.println("The desired option with prefix '" + optionTextPrefix + "' was not found in the Type menu. You may need to update the optionTextPrefix or handle this case accordingly.");
+                }
+
+                // Click on the Add to Cart button
+                
+                lastElement.click();
+
+            }
         } else {
             System.out.println("No elements found with the class 'add-to-cart'.");
         }
+
         // Click on View cart button
-        wait = new WebDriverWait(driver, Duration.ofSeconds(50));
-        WebElement clickViewCartButton = driver.findElement(By.xpath("//a[normalize-space()='View Cart']"));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        
+        wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+        WebElement clickViewCartButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(("//a[normalize-space()='View Cart']"))));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(100));
         clickViewCartButton.click();
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
-        // View Cart
-        WebElement viewCart = driver.findElement(By.xpath("//span[@id='cartItemCountSpan']"));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(50));
-        viewCart.click();
         
         // 5. Empty cart
         
-        WebElement emptyCart = driver.findElement(By.xpath("//button[contains(text(),'Empty Cart')]"));
+        WebElement emptyCart = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(("//button[contains(text(),'Empty Cart')]"))));
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
         emptyCart.click();
+        
         // Assertion to check if the Cart page is displayed
-        WebElement cartHeader = driver.findElement(By.xpath("//h1[normalize-space()='Cart']"));
+        
+        WebElement cartHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[normalize-space()='Cart']")));
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
         Assert.assertEquals(cartHeader.getText(), "Cart", "Cart page is not displayed.");
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        
         // Assertion to check if the Empty Cart Title is displayed
-        WebElement emptyCartTitle = driver.findElement(By.xpath("//h2[@id='empty-cart-title']"));
+        
+        WebElement emptyCartTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(("//h2[@id='empty-cart-title']"))));
         String actualEmptyCartTitle = emptyCartTitle.getText();
         String expectedEmptyCartTitle = "Empty Cart";
         Assert.assertEquals(actualEmptyCartTitle, expectedEmptyCartTitle, "Empty Cart Title is not displayed correctly.");
+        
         // Click on Empty Cart button
-        wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        
+        //Note: I tried to use the xpath locator //button[contains(text(),'Empty Cart')] but it did not work. I also tried the JavaSScriptExecutor, Actions class , still not working
+        
         WebElement emptyCartButton = driver.findElement(By.xpath("/html[1]/body[1]/div[11]/div[1]/div[1]/div[1]/footer[1]/button[1]"));
-        emptyCartButton.click();
-        // Assertion to check if the Cart is Empty Cart
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
-        WebElement emptyCartElement = driver.findElement(By.xpath("//p[contains(text(),'Your cart is empty.')]"));
+        emptyCartButton.click();
+       
+        // Assertion to check if the Cart is Empty Cart
+        
+        wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        WebElement emptyCartElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(),'Your cart is empty.')]")));
         wait = new WebDriverWait(driver, Duration.ofSeconds(100));
         String actualEmptyCartText = emptyCartElement.getText();
         String expectedEmptyCartText = "Your cart is empty.";
         Assert.assertEquals(actualEmptyCartText, expectedEmptyCartText, "The cart is not empty.");
         
         // Close the browser
+        
         driver.quit();
         
 		} catch (Exception e) {
